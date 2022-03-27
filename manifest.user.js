564129cd575080b15235053e3b5373387e5b3c75
Kaneko <mail.qt.kaneko@gmail.com>
2022-03-27 20:51:51 +0300

    Add script managers compatibility
    Remove Firefox manifest
    Add manifest for script managers

---
+++ manifest.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Shikiplayer
+// @namespace https://github.com/qt-kaneko/Shikiplayer
+// @description Adds Kodik player to shikimori.one website.
+// @icon https://github.com/qt-kaneko/Shikiplayer/blob/script/media/icons/icon128.png?raw=true
+// @author Kaneko Qt
+// @homepageURL https://github.com/qt-kaneko/Shikiplayer
+// @match *://shikimori.one/*
+// @require https://raw.githubusercontent.com/qt-kaneko/Shikiplayer/script/config.js
+// @require https://raw.githubusercontent.com/qt-kaneko/Shikiplayer/script/scripts/helpers/helpers.js
+// @require https://raw.githubusercontent.com/qt-kaneko/Shikiplayer/script/scripts/helpers/shikimori.js
+// @require https://raw.githubusercontent.com/qt-kaneko/Shikiplayer/script/scripts/reloader.js
+// @require https://raw.githubusercontent.com/qt-kaneko/Shikiplayer/script/scripts/player.js
+// @grant none
+// @version 2.2.2
+// @copyright 2022, Kaneko Qt
+// @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+// ==/UserScript==
